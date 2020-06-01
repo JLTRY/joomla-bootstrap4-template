3cca1fd2d1c1995e57c3a13567bd50838748cc8f
JLTRY <jltryoen@wanadoo.fr>
2020-06-01 14:35:22 +0200

    adapt templatemod

--- bootstrap4/html/mod_login/submitbutton.js
+++ bootstrap4/html/mod_login/submitbutton.js
@@ -3,7 +3,7 @@
 function googlelog()
 {
 	jQuery('.mod-login input[name="option"]').val("com_jgoogle");
-	jQuery('.mode-login').attr('action', '/index.php?option=com_jgoogle&task=user.login&XDEBUG_SESSION_START=test');
+	jQuery('.mod-login').attr('action', '/index.php?option=com_jgoogle&task=user.login&XDEBUG_SESSION_START=test');
 	Joomla.submitbutton('user.login');
 	//Joomla.submitform('user.login');
 }
